13ca7e60082584e7f9bc4011bc28af6503543322
William Rene Moreno <wmoreno@esri.co>
2020-03-04 09:22:16 -0500

    Ajustes Ejemplos

--- src/tutoriales/js/tutorial_15.js
+++ src/tutoriales/js/tutorial_15.js
@@ -1,9 +1,11 @@
 var dojoConfig = (function(){
-    var base = "https://esri-es.github.io/Static-Maps-API-ArcGIS"
+    // var base = "https://esri-es.github.io/Static-Maps-API-ArcGIS"
+    var base = "https://raw.githubusercontent.com/renemorenow/ArcGIS-JavaScriptAPI/gh-pages/src/tutoriales"
     return {
         async: true,
         isDebug: true,
         packages:[{
+            // name: 'esriES', location: base + '/js', main: "staticmap"
             name: 'esriES', location: base + '/js', main: "staticmap"
         }]
     };
